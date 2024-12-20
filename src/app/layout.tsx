088e6928f7dd1f5a514b7a760875f2dca64c9149
kmcode <kmcode@kmpc>
2024-12-20 18:40:48 +0200

    Initial Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
 import type { Metadata } from "next";
-import { Roboto_Slab, Roboto_Condensed, Reddit_Sans, Reddit_Sans_Condensed } from "next/font/google";
+// import { Roboto_Slab, Roboto_Condensed, Reddit_Sans, Reddit_Sans_Condensed } from "next/font/google";
 import "./globals.css";
 
 
 
-export const robotoSlab = Roboto_Slab({
-  variable: "--font-roboto-slab",
-  subsets: ["latin"]
-});
+// export const robotoSlab = Roboto_Slab({
+//   variable: "--font-roboto-slab",
+//   subsets: ["latin"]
+// });
 
-export const robotoCond = Roboto_Condensed({
-  variable: "--font-roboto-condensed",
-  subsets: ["latin"]
-});
+// export const robotoCond = Roboto_Condensed({
+//   variable: "--font-roboto-condensed",
+//   subsets: ["latin"]
+// });
 
-export const roboto = Reddit_Sans({
-  variable: "--font-reddit-sans",
-  subsets: ["latin"]
-});
+// export const roboto = Reddit_Sans({
+//   variable: "--font-reddit-sans",
+//   subsets: ["latin"]
+// });
 
-export const redditCond = Reddit_Sans_Condensed({
-  variable: "--font-reddit-sans-condensed",
-  subsets: ["latin"]
-});
+// export const redditCond = Reddit_Sans_Condensed({
+//   variable: "--font-reddit-sans-condensed",
+//   subsets: ["latin"]
+// });
 
 
 export const metadata: Metadata = {
